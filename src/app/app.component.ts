f48bc8c0fc6326dbb1e315e22b413eb513c6587c
didosaurus <dian_t_d@yahoo.co.uk>
2021-05-24 09:56:59 +0100

    gitignore allowed dist folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,6 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
     constructor(translate: TranslateService) {
-        translate.setDefaultLang('en');
+        translate.setDefaultLang(localStorage.getItem('lcid')?.slice(0, 2) || 'en');
     }
 }
