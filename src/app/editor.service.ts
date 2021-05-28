4d915fb0d185a9b268aeecfa68c81e3665203378
didosaurus <dian_t_d@yahoo.co.uk>
2021-05-28 09:42:40 +0100

    Audio context check

--- src/app/editor.service.ts
+++ src/app/editor.service.ts
@@ -1,25 +1,24 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, fromEvent } from 'rxjs';
 import { ScriptService } from './script.service';
-import { LangsService } from './langs.service';
 
-const codes = ['ar', 'cs-cz', 'de-de', 'en-us', 'es-es', 'fi-fi', 'fr-fr', 'gl-es', 'it-it', 'ja-jp', 'ko-kr', 'nb-no', 'nl-nl', 'pl-pl', 'ru-ru', 'sv-se', 'tr-tr', 'uk-ua', 'zh-cn', 'zh-tw'];
+const codes = ['ar', 'cs-cz', 'de-de', 'es-es', 'fi-fi', 'fr-fr', 'gl-es', 'it-it', 'ja-jp', 'ko-kr', 'nb-no', 'nl-nl', 'pl-pl', 'ru-ru', 'sv-se', 'tr-tr', 'uk-ua', 'zh-cn', 'zh-tw'];
 
 @Injectable({ providedIn: 'root' })
 export class EditorService {
     scriptSoruce = new BehaviorSubject<string>('');
-    constructor(private scriptService: ScriptService) {
-        const iso = localStorage.getItem('iso');
-        if (iso) {
-            const iso6391 = iso.slice(0, 2);
+    constructor(scriptService: ScriptService) {
+        const lcid = localStorage.getItem('lcid') ?? navigator.language;
+        if (lcid) {
+            const iso6391 = lcid.slice(0, 2);
             const i18nFile = codes.find(code => code.startsWith(iso6391));
             console.log(100, i18nFile);
             if (i18nFile) {
-                fromEvent(scriptService.add(`https://uicdn.toast.com/editor/latest/i18n/${i18nFile}.js`), 'load')
+                fromEvent(scriptService.add(`https://uicdn.toast.com/editor/latest/i18n/${i18nFile}.min.js`), 'load')
                     .subscribe(() => this.scriptSoruce.next(i18nFile));
-                /* scriptService.add(`https://uicdn.toast.com/editor/latest/i18n/${i18nFile}.js`).onload = () => {
-                    console.log('SkyScanner Tag loaded');
-                } */ 
+            }
+            else {
+                this.scriptSoruce.next('en-us');
             }
         }
     }
